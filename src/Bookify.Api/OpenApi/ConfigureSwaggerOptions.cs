c3a0391a2bce82f83463eb81bb1a5f4d4f5a30fe
Emad80Afrasyabi <mohammademadafrasyabi@gmail.com>
2025-08-28 22:39:57 +0330

    Add authorize section for swaggerUi

--- src/Bookify.Api/OpenApi/ConfigureSwaggerOptions.cs
+++ src/Bookify.Api/OpenApi/ConfigureSwaggerOptions.cs
@@ -11,6 +11,31 @@ public sealed class ConfigureSwaggerOptions(IApiVersionDescriptionProvider provi
     {
         foreach (ApiVersionDescription description in provider.ApiVersionDescriptions)
             options.SwaggerDoc(description.GroupName, CreateVersionInfo(description));
+
+        // Add JWT Bearer authentication
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                Array.Empty<string>()
+            }
+        });
     }
 
     public void Configure(string? name, SwaggerGenOptions options)
