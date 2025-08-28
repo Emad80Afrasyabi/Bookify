59e862e93499c78e979837edef96501985c78d52
Emad80Afrasyabi <mohammademadafrasyabi@gmail.com>
2025-08-28 19:54:04 +0330

    Add distributed cache (Redis)

--- src/Bookify.Infrastructure/Authorization/AuthorizationService.cs
+++ src/Bookify.Infrastructure/Authorization/AuthorizationService.cs
@@ -1,12 +1,19 @@
-﻿using Bookify.Domain.Users;
+﻿using Bookify.Application.Abstractions.Caching;
+using Bookify.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure.Authorization;
 
-internal sealed class AuthorizationService(ApplicationDbContext dbContext)
+internal sealed class AuthorizationService(ApplicationDbContext dbContext, ICacheService cacheService)
 {
     public async Task<UserRolesResponse> GetRolesForUserAsync(string identityId)
     {
+        var cacheKey = $"auth:roles-{identityId}";
+        var cachedRoles = await cacheService.GetAsync<UserRolesResponse>(cacheKey);
+
+        if (cachedRoles is not null)
+            return cachedRoles;
+        
         UserRolesResponse roles = await dbContext.Set<User>().Where(user => user.IdentityId == identityId)
                                                              .Select(user => new UserRolesResponse
                                                              {
@@ -14,16 +21,26 @@ internal sealed class AuthorizationService(ApplicationDbContext dbContext)
                                                                  Roles = user.Roles.ToList()
                                                              })
                                                              .FirstAsync();
+        
+        await cacheService.SetAsync(cacheKey, roles);
 
         return roles;
     }
     
     public async Task<HashSet<string>> GetPermissionsForUserAsync(string identityId)
     {
+        var cacheKey = $"auth:permissions-{identityId}";
+        var cachedPermissions = await cacheService.GetAsync<HashSet<string>>(cacheKey);
+
+        if (cachedPermissions is not null)
+            return cachedPermissions;
+        
         ICollection<Permission> permissions = await dbContext.Set<User>()
             .Where(user => user.IdentityId == identityId)
             .SelectMany(user => user.Roles.Select(r => r.Permissions))
             .FirstAsync();
+        
+        await cacheService.SetAsync(cacheKey, permissions);
 
         return permissions.Select(permission => permission.Name).ToHashSet();
     }
--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Application.Abstractions.Authentication;
+using Bookify.Application.Abstractions.Caching;
 using Bookify.Application.Abstractions.Clock;
 using Bookify.Application.Abstractions.Data;
 using Bookify.Application.Abstractions.Email;
@@ -8,6 +9,7 @@ using Bookify.Domain.Bookings;
 using Bookify.Domain.Users;
 using Bookify.Infrastructure.Authentication;
 using Bookify.Infrastructure.Authorization;
+using Bookify.Infrastructure.Caching;
 using Bookify.Infrastructure.Clock;
 using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Email;
@@ -106,5 +108,14 @@ public static class DependencyInjection
         services.AddTransient<IAuthorizationHandler, PermissionAuthorizationHandler>();
 
         services.AddTransient<IAuthorizationPolicyProvider, PermissionAuthorizationPolicyProvider>();
+    }
+    
+    private static void AddCaching(IServiceCollection services, IConfiguration configuration)
+    {
+        string connectionString = configuration.GetConnectionString(name: "Cache") ?? throw new ArgumentNullException(nameof(configuration));
+
+        services.AddStackExchangeRedisCache(options => options.Configuration = connectionString);
+
+        services.AddSingleton<ICacheService, CacheService>();
     }
 }
